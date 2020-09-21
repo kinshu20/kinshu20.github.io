042c58a4388540cac6d303d06e409813956a1e5b
kinshu20 <1by18is109@bmsit.in>
2020-09-21 17:11:13 +0530

    3rd commit

--- blogger/js/main.js
+++ blogger/js/main.js
@@ -1,3 +1,17 @@
+const responsive={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    } 
+}
 $(document).ready(function(){
 
     $nav=$('.nav');
@@ -12,6 +26,36 @@ $(document).ready(function(){
 
 //owl-crousel for blog
 
-$('.owl-carousel').owlCarousel();
+if($(window).width()>=721)
+{
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        responsive:responsive,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    });
+     
+}
+else if($(window).width()<=720){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        responsive:responsive,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    });
+}
+
+//click to go above
+
+$('.move-up span').click(function(){
+    $('html,body').animate({
+        scrollTop:0
+    },1000);
+})
 
 });
